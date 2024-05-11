49665db3f71a992c8a88c263833f8578027ba0af
Rartii <47690543+Rartii@users.noreply.github.com>
2024-05-11 18:56:14 +0200

    Database fix

--- database.sql
+++ database.sql
@@ -9,11 +9,22 @@ CREATE TABLE `users`(
     `lastname` varchar(45) NOT NULL default '',
     `email` varchar(50) NOT NULL default '',
     `username` varchar(30) NOT NULL default '',
-    `password` varchar(30) NOT NULL default '',
+    `password` varchar(255) NOT NULL default '',
     PRIMARY KEY (`user_id`)
 )
 ENGINE = MYISAM
 CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE `feedback`(
+    `feedback_id` int(10) unsigned NOT NULL auto_increment,
+    `user` varchar(91) NOT NULL default 'Vendég',
+    `message` varchar(2000) NOT NULL default '',
+    `date_add` DATETIME NOT NULL default CURRENT_TIMESTAMP(),
+    PRIMARY KEY (`feedback_id`)
+)
+ENGINE = MYISAM
+CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 
 INSERT INTO `users`(`user_id`,`firstname`,`lastname`,`email`,`username`, `password`) VALUES
 (1, 'admin', 'admin','admin@gmail.com','admin', sha1('admin123'));
